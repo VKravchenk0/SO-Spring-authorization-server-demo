71caaa74d8aaa6fb4430f89390a1c2637c8c81ce
Viacheslav Kravchenko <vk@consulting.mk>
2024-01-02 08:47:13 +0200

    Initial commit

---
+++ src/main/java/com/github/vkravchenk0/demo/AuthorizationServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.vkravchenk0.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vkravchenk0/demo/AuthorizationServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vkravchenk0.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
